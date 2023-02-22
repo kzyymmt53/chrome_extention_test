ce8463c9ac949548b7f4434f67d26653450b13de
kzyymmt53 <kzyymmt53@gmail.com>
2023-02-22 19:09:14 +0900

    feat: プルダウンで選んだ値を保存getBucketの第二引数をsyncにするとログインユーザでデータを同期、localにすると使用ブラウザのみ

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,7 +1,30 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
+import { getBucket } from '@extend-chrome/storage';
 import { Container, Select } from '@mantine/core';
 
+interface MyBucket {
+  targetLang: string;
+}
+
+const bucket = getBucket<MyBucket>('my_bucket', 'sync');
+
 const Popup = (): ReactElement => {
+  const [lang, setLang] = useState('EN');
+
+  useEffect(() => {
+    (async () => {
+      const value = await bucket.get();
+      if (value.targetLang) {
+        setLang(value.targetLang);
+      }
+    })();
+  }, []);
+
+  const saveLang = (lang: string) => {
+    bucket.set({ targetLang: lang });
+    setLang(lang);
+  };
+
   document.body.style.width = '20rem';
   document.body.style.height = '20rem';
   return (
@@ -9,6 +32,8 @@ const Popup = (): ReactElement => {
       <Select
         label="どの言語に翻訳しますか？"
         defaultValue="EN"
+        onChange={(value: string) => saveLang(value)}
+        value={lang}
         data={[
           { value: 'EN', label: '英語' },
           { value: 'KO', label: '韓国語' },
